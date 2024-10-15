8df5f239bee2b085a6938393ccb0476e86113f53
ECeles <efraim.jr30@gmail.com>
2024-10-14 21:29:21 -0300

    movimentacao dos thumbs com a imagem de fundo e seus textos correlatos

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 let btnNext = document.querySelector(".next");
-let btnBack = document.querySelector(".prev");
+let btnPrev = document.querySelector(".prev");
 
 let container = document.querySelector(".container");
 let list = document.querySelector(".container .list");
 let thumb = document.querySelector(".container .thumb");
+
+btnNext.onclick = () => moveItemsOnClick("next");
+btnPrev.onclick = () => moveItemsOnClick("prev");
+
+function moveItemsOnClick(type) {
+  let listItems = document.querySelectorAll(".list .listItems");
+  let thumbItems = document.querySelectorAll(".thumb .thumbItems");
+
+  if (type === "next") {
+    list.appendChild(listItems[0]);
+    thumb.appendChild(thumbItems[0]);
+  } else {
+  }
+}
